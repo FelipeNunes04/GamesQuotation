c73414f8181348cb8303282968ddb7209213b4fe
felipenunes04 <felipennunes@hotmail.com>
2017-03-27 10:23:28 -0300

    debug False

--- cotacao/settings.py
+++ cotacao/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#_)2aj#&rmb9fes2vmr+94k&bl1g58s8pw-w*qn$7k!l3gl5!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
