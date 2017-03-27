f607fc53d2dc23c0b8cfa98776cb6a0baf7ceaf4
felipenunes04 <felipennunes@hotmail.com>
2017-03-27 10:39:27 -0300

    debug False

--- cotacao/settings.py
+++ cotacao/settings.py
@@ -124,4 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+
+    ('assets', os.path.join(PROJECT_DIR, '../static')),
+)
